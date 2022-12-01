c7d30df6f78fc9161a6ebce8ad52bb8d0d2ea63d
4ra1n <yiqing.xu@chaitin.com>
2022-12-01 17:38:30 +0800

    test

--- src/main/java/com/chaitin/jar/analyzer/test/CFRTest.java
+++ src/main/java/com/chaitin/jar/analyzer/test/CFRTest.java
@@ -3,7 +3,9 @@ package com.chaitin.jar.analyzer.test;
 public class CFRTest {
     public static void main(String[] a) {
         String[] args = new String[]{
-                "Application.class"
+                "Application.class",
+                "--outputpath",
+                "1.txt"
         };
         org.benf.cfr.reader.Main.main(args);
     }
